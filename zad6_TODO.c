8b176a6a5e0c0088ff67f12032bec2676883a1ef
szymbaramichal <szymbaramichal@gmail.com>
2023-06-12 14:30:26 +0200

    todo tasks

---
+++ zad6_TODO.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 21
+
+typedef struct towar
+{
+    unsigned int id;
+    char nazwa[N];
+    float cena;
+} TOWAR;
+
+int main(int argc, char *argv[])
+{
+    TOWAR produkt = {.nazwa = "pralka", .cena = 899.99, .id = 1};
+    TOWAR kopia_produkt;
+
+    FILE *plik = fopen(argv[1], "rw");
+    if(plik == NULL)
+    {
+        return 0;
+    }
+
+    fprintf(plik, "s");
+
+    printf("id: %u nazwa: %s cena %.2f zl",
+           kopia_produkt.id, kopia_produkt.nazwa, kopia_produkt.cena);
+
+
+    fclose(plik);
+
+    return 0;
+}

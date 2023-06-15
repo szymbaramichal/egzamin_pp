34fac839c1f5d915db7955e6cb870b7e5bb82e5c
szymbaramichal <szymbaramichal@gmail.com>
2023-06-15 18:11:34 +0200

    zad3 fix

--- zad3.c
+++ zad3.c
@@ -3,18 +3,17 @@
 
 int liczba_podciagow(char* lancuch1, char* lancuch2) {
     int count = 0;
-    char* ptr = lancuch1;
 
-    while ((ptr = strstr(ptr, lancuch2)) != NULL) {
+    while ((lancuch1 = strstr(lancuch1, lancuch2)) != NULL) {
         count++;
-        ptr += strlen(lancuch2);
+        lancuch1 += strlen(lancuch2);
     }
 
     return count;
 }
 
 int main() {
-    char lancuch1[] = "mamama";
+    char lancuch1[] = "zxczxczxczczxczxczxcmamama";
     char lancuch2[] = "mam";
     int wynik = liczba_podciagow(lancuch1, lancuch2);
     printf("Liczba wystapien: %d\n", wynik);
